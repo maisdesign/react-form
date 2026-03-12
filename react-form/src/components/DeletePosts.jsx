4d688ffe6d40ef0c5a06cdc961d9392bf7829a37
Maisdesign <downloadtaky@gmail.com>
2026-03-12 01:49:07 +0100

    Sistemata estetica dei bottoni e del sito in generale

--- react-form/src/components/DeletePosts.jsx
+++ react-form/src/components/DeletePosts.jsx
@@ -1,7 +1,7 @@
 function DeletePosts({ erasePost, deletePost, setDeletePost }) {
 
     return <>
-        <button className="btn btn-danger post-delete" onClick={() => erasePost(deletePost)}>
+        <button className="btn btn-danger post-delete mx-1" onClick={() => erasePost(deletePost)}>
             Cancellalo</button >
         <button type="button" className="btn btn-outline-danger" onClick={() => setDeletePost(null)}>Ripensaci
         </button >
--- react-form/src/components/PostItem.jsx
+++ react-form/src/components/PostItem.jsx
@@ -9,20 +9,18 @@ function PostItem({ article, updatePost, erasePost }) {
 
     return <>
         <article className="single-post">
-            <h2 className="post-title">
-                {article.title}
-            </h2>
-            <button type="button" className="btn btn-primary" onClick={() => setIsOpen(!isOpen)}>{isOpen ? "Read less" : "Read more"}</button>
-            <div className="post-body">
-                {isOpen && article.body}
+            <div className="post-header">
+                <h2 className="post-title">{article.title}</h2>
+                <div className="post-actions gap-1">
+                    <button type="button" className="btn btn-sm btn-outline-warning" onClick={() => setEditSinglePost(article.id)}><i className="bi bi-pencil-square"></i></button>
+                    <button type="button" className="btn btn-sm btn-outline-danger" onClick={() => setDeleteSinglePost(article.id)}><i className="bi bi-x-square"></i></button>
+                </div>
             </div>
-            <button type="button" className="btn btn-warning" onClick={() => setEditSinglePost(article.id)}><i className="bi bi-pencil-square"></i>
-            </button>
-            <button type="button" className="btn btn-danger" onClick={() => setDeleteSinglePost(article.id)}><i className="bi bi-x-square"></i>
-            </button>
+            <button type="button" className="btn btn-sm btn-outline-secondary" onClick={() => setIsOpen(!isOpen)}>{isOpen ? "Read less" : "Read more"}</button>
+            {isOpen && <div className="post-body">{article.body}</div>}
+            {editSinglePost !== null && <ModifyPosts toModify={article} setEditPost={setEditSinglePost} updatePost={updatePost} />}
+            {deleteSinglePost !== null && <DeletePosts deletePost={deleteSinglePost} erasePost={erasePost} setDeletePost={setDeleteSinglePost} />}
         </article>
-        {editSinglePost !== null && <ModifyPosts toModify={article} setEditPost={setEditSinglePost} updatePost={updatePost} />}
-        {deleteSinglePost !== null && <DeletePosts deletePost={deleteSinglePost} erasePost={erasePost} setDeletePost={setDeleteSinglePost} />}
     </>
 }
 
